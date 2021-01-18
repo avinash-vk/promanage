8be6c4b814e9174c7e5eb811868bd764765a0b32
Sanskriti Pattanayk <sanskriti.pattanayak@gmail.com>
2021-01-18 22:25:25 +0530

    Fonts

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+const useStyles = makeStyles({
 
+});
 const Dashboard = () => {
+    const classes = useStyles();
     return (
-        <div>
-            Dashboard
-        </div>
+    <div>
+          Dashboard
+    </div>
     )
 }
-
 export default Dashboard
