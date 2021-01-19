0fc8378c0b46b99865cd21a20eadc39607529463
avinash-vk <avinash2000vk@gmail.com>
2021-01-19 15:31:22 +0530

    added form content

--- client/src/components/Modals.js
+++ client/src/components/Modals.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Modal, Avatar, Typography } from '@material-ui/core';
+import { Modal, Avatar, Typography, TextField, Divider } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { ColorButton } from './Button';
 
@@ -30,6 +30,9 @@ const modalStyles = makeStyles((theme)=>({
         flexDirection:'row',
         alignItems:'center',
         justifyContent:'center',
+        borderBottom:"2px solid #A7A7A7",
+        width:"100%",
+        paddingBottom:8
     },
     logo:{
         marginRight:12,
@@ -39,7 +42,6 @@ const modalStyles = makeStyles((theme)=>({
     content:{
         height:520,
         width:"100%",
-        backgroundColor:"#F4F4F4",
         borderRadius:10,
         overflowY:"auto",
         display:'flex',
@@ -52,6 +54,19 @@ const modalStyles = makeStyles((theme)=>({
         borderRadius:100,
         paddingLeft:30,
         paddingRight:30
+    },
+    inputContainer:{
+        marginBottom:40,
+        display:'flex',
+        justifyContent:'center',
+        flexDirection:'column',
+    },
+    input:{
+        fontFamily: "Poppins",
+        backgroundColor: "#E5E5E5",
+        borderRadius: 8,
+        padding: 10,
+        width:580
     }
 }))
 
@@ -71,7 +86,7 @@ export const GithubModal = ({open, handleClose}) => {
                         Import from Github
                     </Typography>
                 </div>
-                <div className={classes.content}>
+                <div className={classes.content} style={{backgroundColor:"#F4F4F4"}}>
                     This is not integrated yet.
                 </div>
                 <div className={classes.footer}>
@@ -92,7 +107,43 @@ export const CreateProjectModal = ({open,handleClose}) => {
             onClose={handleClose}
             className={classes.modal}
         >
-            <div className={classes.root} >
+            <div className={classes.root}>
+                <div className={classes.header}>
+                    <Typography className={classes.text}>
+                        Start your project from scratch
+                    </Typography>
+                </div>
+                <div className={classes.content}>
+                    <form style={{width:"100%"}}>
+                        <div className={classes.inputContainer}>
+                            <Typography className={classes.text}>
+                                Title
+                            </Typography>
+                            <TextField 
+                                fullWidth
+                                InputProps={{disableUnderline: true}}
+                                className={classes.input} 
+                            />
+                        </div>
+                        <div className={classes.inputContainer}>
+                            <Typography className={classes.text}>
+                                Description
+                            </Typography>
+                            <TextField 
+                                fullWidth
+                                multiline 
+                                rows={8} 
+                                InputProps={{disableUnderline: true}}
+                                className={classes.input} 
+                            />
+                        </div>
+                    </form>
+                </div>
+                <div className={classes.footer}>
+                    <ColorButton variant="contained" color="primary" onClick={()=>{}} className={classes.button}>
+                        Create
+                    </ColorButton>
+                </div>
             </div>
         </Modal>
     )
