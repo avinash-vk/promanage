eadfaeb4c0d32c3a7a33357a0ff821c750c3d0ae
avinash-vk <avinash2000vk@gmail.com>
2021-01-21 20:59:24 +0530

    added create project

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -5,7 +5,7 @@ const Projects = {
     getProject : async (id) => (await axios.get(URL.project(id))).data,
     updateProject : async (id,params) => (await axios.put(URL.project(id),params)).data,
     deleteProject : async (id) => (await axios.delete(URL.project(id))).data,
-    createProject : async (id,params) => (await axios.post(URL.createProject,params)).data,
+    createProject : async (id,params) => (await axios.post(URL.createProject(),params)).data,
     getProjectForUser : async (id) => (await axios.get(URL.getProjects(id))).data,
 }
 let API;
