d374d2f4b18a258fd0f891d0bd83a2d8c16b46f7
avinash-vk <avinash2000vk@gmail.com>
2021-01-24 14:04:23 +0530

    added get projects from api

--- cli/src/commands/get.js
+++ cli/src/commands/get.js
@@ -1,17 +1,22 @@
 import inquirer from 'inquirer';
+import API from '../api'; 
 
 export const get = async (args) => {
+    // TODO AUTHENTICATE THIS BRO
+    const id = "9Jka8ovtPja2nwQCnRDyf5LtIqe2";
+
+    let projects = (await API.getProjects(id)).data.projects;
+
+    let choices = projects.map(project => project.title);
+    let ids = projects.reduce((res,cur) => ({...res,[cur.title]:cur.id}),{});
     inquirer.prompt([
         {
             type:'list',
             name: 'project',
             message: 'Which project are you working on?',
-            choices: [
-                'meraki',
-                'this is great'
-            ],
+            choices: choices,
         }
     ]).then(answers => {
-        console.info('Answer',answers.project)
+        console.info('Answer',ids[answers.project])
     })
 }
