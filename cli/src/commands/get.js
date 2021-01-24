1fd714a6fb04c1bc3ffcee41b6ea03eebc62e9af
avinash-vk <avinash2000vk@gmail.com>
2021-01-24 14:17:31 +0530

    completed placing file

--- cli/src/commands/get.js
+++ cli/src/commands/get.js
@@ -1,5 +1,14 @@
 import inquirer from 'inquirer';
+import { promises as fs } from 'fs';
 import API from '../api'; 
+
+const writeEnv = async (envString) => {
+    try {
+      await fs.writeFile('.env',envString);
+    } catch (error) {
+      console.error(`Got an error trying to write to a file: ${error.message}`);
+    }
+  }
 
 export const get = async (args) => {
     // TODO AUTHENTICATE THIS BRO
@@ -8,7 +17,7 @@ export const get = async (args) => {
     let projects = (await API.getProjects(id)).data.projects;
 
     let choices = projects.map(project => project.title);
-    let ids = projects.reduce((res,cur) => ({...res,[cur.title]:cur.id}),{});
+    let ids = projects.reduce((res,cur) => ({...res,[cur.title]:{...cur}}),{});
     inquirer.prompt([
         {
             type:'list',
@@ -16,7 +25,19 @@ export const get = async (args) => {
             message: 'Which project are you working on?',
             choices: choices,
         }
-    ]).then(answers => {
-        console.info('Answer',ids[answers.project])
+    ]).then(async answers => {
+        console.info("Downloading env variables for ",answers.project);
+        let project = ids[answers.project];
+        let envString = "";
+        if (!project.env){
+            console.log("No env variables defined for this project! Update it at promanage")
+        }
+        else {
+            project.env.forEach(variable => {
+                envString+=variable.key+"="+variable.value+"\n";
+            })
+            await writeEnv(envString);
+            console.info("✅ Done");
+        }
     })
 }
