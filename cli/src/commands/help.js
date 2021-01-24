0908729ecc746ae3af3b9957b336da83dda99e14
avinash-vk <avinash2000vk@gmail.com>
2021-01-24 12:56:46 +0530

    added help

--- cli/src/commands/help.js
+++ cli/src/commands/help.js
@@ -1,3 +1,25 @@
+import chalk from 'chalk';
+
+const menus = {
+  main: `
+    ${chalk.greenBright('promanage [command] <options>')}
+    ${chalk.blueBright('get')} ................ get your env for your specified project
+    ${chalk.blueBright('config')}.............. login to your promanage account
+    ${chalk.blueBright('version')} ............ show package version
+    ${chalk.blueBright('help')} ............... show help menu for a command
+  `,
+
+  get:`
+    //....
+  `,
+
+  config: `//...
+        `,
+}
+
 export const help = (args) => {
-    console.log("help ",args);
+    const subCmd = args._[0] === 'help'
+    ? args._[1]
+    : args._[0]
+    console.log(menus[subCmd] || menus.main)
 }
