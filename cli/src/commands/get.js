3d13ebc1f13dbc70aa0fe08db6148b7857a6c8e8
avinash-vk <avinash2000vk@gmail.com>
2021-01-24 13:41:13 +0530

    added interactive get

--- cli/src/commands/get.js
+++ cli/src/commands/get.js
@@ -1,3 +1,17 @@
-export const get = (args) => {
-    console.log("get ",args);
+import inquirer from 'inquirer';
+
+export const get = async (args) => {
+    inquirer.prompt([
+        {
+            type:'list',
+            name: 'project',
+            message: 'Which project are you working on?',
+            choices: [
+                'meraki',
+                'this is great'
+            ],
+        }
+    ]).then(answers => {
+        console.info('Answer',answers.project)
+    })
 }
