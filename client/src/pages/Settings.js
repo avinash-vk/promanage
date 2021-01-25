37ff8754344723ceb3c9cb916e2628648da3d5fd
avinash-vk <avinash2000vk@gmail.com>
2021-01-25 22:34:43 +0530

    added settings filler

--- client/src/pages/Settings.js
+++ client/src/pages/Settings.js
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import {ReactComponent as UnderDev} from '../assets/underDev.svg';
+import { Header } from '../components/Header';
 const Settings = () => {
     return (
-        <div>
-            Settings
+        <div style={{display:"flex",flexDirection:"column",flexGrow:1,}}>
+            <Header title="Settings" />
+            <div style={{display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center"}}>
+                <UnderDev style={{height:500,width:500}} />
+                Looks like you don't have much settings to take control of, we'll be back ASAP
+            </div>
         </div>
     )
 }
