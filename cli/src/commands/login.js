a287ea7f8f2d26bbd47c8571ebacdf8b9631b1ea
avinash-vk <avinash2000vk@gmail.com>
2021-01-25 14:51:54 +0530

    added basic auth functionality

--- cli/src/commands/login.js
+++ cli/src/commands/login.js
@@ -1,6 +1,25 @@
-import { googleSignin } from '../auth/methods';
+import Conf from 'conf';
+import inquirer from 'inquirer';
+import API from '../api';
+
+const config = new Conf();
 
 export const login = (args) => {
-    googleSignin();
-    console.log("login ",args);
+    inquirer.prompt([
+        {
+            name: "creds",
+            message: "Enter the login code copied from promanage:",
+        }
+    ]).then(async answers => {
+        try{
+            let res = await API.getUser(answers.creds);
+            console.log("Logged in as",res.data.username);
+            conf.set("username",res.data.username);
+            conf.set("id", res.data.id);
+        }
+        catch(err){
+            console.log("Oops enter a valid code!")
+            return;
+        }
+    })
 }
